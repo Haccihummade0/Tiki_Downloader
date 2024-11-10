1a60841bf0474e0b128053851c11852af91eb608
Haccihummade0 <77897693+Haccihummade0@users.noreply.github.com>
2024-11-10 23:23:57 +0300

    Update config.py

--- config.py
+++ config.py
@@ -3,5 +3,5 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-BOT_TOKEN = os.environ.get("7845040043:AAFdN-F40r3f687b0aGQGCHJq9XyHyuMGN4")
-MAIN_ADMIN = str(os.environ.get("1271362249")).split(",")
+BOT_TOKEN = os.environ.get(":AAFdN-ljg")
+MAIN_ADMIN = str(os.environ.get("12713622749")).split(",")
